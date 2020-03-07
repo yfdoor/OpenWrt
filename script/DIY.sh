9485a0875852f46b9f95c2bde23c5731087911f7
yfdoor <yfdoor@163.com>
2020-03-07 12:13:40 +0800

    update

--- script/DIY.sh
+++ script/DIY.sh
@@ -1,2 +1,16 @@
+#/bin/bash
 # This is free software, lisence use MIT.
 # Copyright (C) https://github.com/yfdoor
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
+
+uci set luci.main.lang=zh_cn
+uci commit luci
+uci set system.@system[0].timezone=CST-8
+uci set system.@system[0].zonename=Asia/Shanghai
+uci commit system
+
+#sed -i 's/downloads.openwrt.org/op.hyird.xyz/g' /etc/opkg/distfeeds.conf
+#sed -i 's/snapshots/$(cat ../version)/g' /etc/opkg/distfeeds.conf
+#sed -i "s/# //g" /etc/opkg/distfeeds.conf
