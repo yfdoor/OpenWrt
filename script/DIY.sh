e438106bf5d4845f31f80d8484a292c87b624926
YFDOOR <32769281+yfdoor@users.noreply.github.com>
2020-03-07 13:42:40 +0800

    Update DIY.sh

--- script/DIY.sh
+++ script/DIY.sh
@@ -5,11 +5,11 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 
-uci set luci.main.lang=zh_cn
-uci commit luci
-uci set system.@system[0].timezone=CST-8
-uci set system.@system[0].zonename=Asia/Shanghai
-uci commit system
+#uci set luci.main.lang=zh_cn
+#uci commit luci
+#uci set system.@system[0].timezone=CST-8
+#uci set system.@system[0].zonename=Asia/Shanghai
+#uci commit system
 
 #sed -i 's/downloads.openwrt.org/op.hyird.xyz/g' /etc/opkg/distfeeds.conf
 #sed -i 's/snapshots/$(cat ../version)/g' /etc/opkg/distfeeds.conf
