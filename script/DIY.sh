157e09cc2449585a6ef317038361ea50dd9dbcd7
yfdoor <yfdoor@163.com>
2020-03-19 22:44:04 +0800

    update ADG

--- script/DIY.sh
+++ script/DIY.sh
@@ -5,12 +5,7 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 
-#uci set luci.main.lang=zh_cn
-#uci commit luci
-#uci set system.@system[0].timezone=CST-8
-#uci set system.@system[0].zonename=Asia/Shanghai
-#uci commit system
-
-#sed -i 's/downloads.openwrt.org/op.hyird.xyz/g' /etc/opkg/distfeeds.conf
-#sed -i 's/snapshots/$(cat ../version)/g' /etc/opkg/distfeeds.conf
-#sed -i "s/# //g" /etc/opkg/distfeeds.conf
+# Define My Package
+sudo mkdir -p package/yfdoor
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git                 package/yfdoor
+git clone https://github.com/KFERMercer/openwrt-adguardhome.git                     package/yfdoor
