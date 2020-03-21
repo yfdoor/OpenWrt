bd8c94fec12baa084abe1379153424444f20f43c
yfdoor <yfdoor@163.com>
2020-03-21 11:33:15 +0800

    update define config

--- script/DIY.sh
+++ script/DIY.sh
@@ -2,8 +2,31 @@
 # This is free software, lisence use MIT.
 # Copyright (C) https://github.com/yfdoor
 
-# Modify default IP
+# Define IP
 sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 
 # Define My Package
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git                 package/yfdoor/luci-app-adguardhome
+
+# Define Default
+cat > package/lean/default-settings/files/zzz-default-settings <<-EOF
+    #!/bin/sh
+    # set time zone
+    uci set system.@system[0].timezone=CST-8
+    uci set system.@system[0].zonename=Asia/Shanghai
+    uci commit system
+
+    # set distfeeds
+    cp /etc/opkg/distfeeds.conf /etc/opkg/distfeeds.conf_BK
+    sed -i 's#http://downloads.openwrt.org#http://mirrors.tuna.tsinghua.edu.cn/openwrt#g' /etc/opkg/distfeeds.conf
+
+    # set firewall
+    sed -i '/REDIRECT --to-ports 53/d' /etc/firewall.user
+    echo "iptables -t nat -A PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 53" >> /etc/firewall.user
+    echo "iptables -t nat -A PREROUTING -p tcp --dport 53 -j REDIRECT --to-ports 53" >> /etc/firewall.user
+
+    # Clear tmp
+    rm -rf /tmp/luci-*
+
+    exit 0
+EOF
