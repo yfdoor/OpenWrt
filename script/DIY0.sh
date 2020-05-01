6a6157b1a681b46e4acdb30be155bb26d31c1cd8
YFDOOR <32769281+yfdoor@users.noreply.github.com>
2020-05-01 21:37:22 +0800

    Update and rename DIY.sh to DIY0.sh

---
+++ script/DIY0.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+# This is free software, lisence use MIT.
+# Copyright (C) https://github.com/yfdoor
+
+# Define My Package
+git clone https://github.com/yfdoor/openwrt-adguardhome.git                         package/yfdoor/adguardhome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git                 package/yfdoor/luci-app-adguardhome
+git clone https://github.com/fw876/helloworld.git                                   package/yfdoor/luci-app-ssr-plus
