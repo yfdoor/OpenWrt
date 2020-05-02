df62c1dc3949f6fc5f4d020f6a98f3fb7f7c2a4e
YFDOOR <32769281+yfdoor@users.noreply.github.com>
2020-05-02 10:56:47 +0800

    Update DIY0.sh

--- script/DIY0.sh
+++ script/DIY0.sh
@@ -3,6 +3,6 @@
 # Copyright (C) https://github.com/yfdoor
 
 # Define My Package
-git clone https://github.com/yfdoor/OpenWrt-Adguardhome.git                         package/yfdoor/adguardhome
+git clone https://github.com/yfdoor/OpenWrt-AdGuardHome.git                         package/yfdoor/adguardhome
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git                 package/yfdoor/luci-app-adguardhome
 git clone https://github.com/fw876/helloworld.git                                   package/yfdoor/luci-app-ssr-plus
