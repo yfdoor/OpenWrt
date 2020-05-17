73e139012c2b78a1a8086e60db0bdd827dc233f2
YFDOOR <32769281+yfdoor@users.noreply.github.com>
2020-05-17 20:34:44 +0800

    Update DIY1.sh

--- script/DIY1.sh
+++ script/DIY1.sh
@@ -12,6 +12,7 @@ cat > package/lean/default-settings/files/zzz-default-settings <<-EOF
     uci set system.@system[0].timezone=CST-8
     uci set system.@system[0].zonename=Asia/Shanghai
     uci commit system
+    
     # set distfeeds
     cp /etc/opkg/distfeeds.conf /etc/opkg/distfeeds.conf_BK
     sed -i 's#http://downloads.openwrt.org#http://mirrors.tuna.tsinghua.edu.cn/openwrt#g' /etc/opkg/distfeeds.conf
@@ -21,7 +22,15 @@ cat > package/lean/default-settings/files/zzz-default-settings <<-EOF
     sed -i '/REDIRECT --to-ports 53/d' /etc/firewall.user
     echo "iptables -t nat -A PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 53" >> /etc/firewall.user
     echo "iptables -t nat -A PREROUTING -p tcp --dport 53 -j REDIRECT --to-ports 53" >> /etc/firewall.user
-    # clear tmp
+    
+    # Others
+    sed -i '/log-facility/d' /etc/dnsmasq.conf
+    echo "log-facility=/dev/null" >> /etc/dnsmasq.conf
+
+    sed -i 's/cbi.submit\"] = true/cbi.submit\"] = \"1\"/g' /usr/lib/lua/luci/dispatcher.lua
+
+    echo 'hsts=0' > /root/.wgetrc
+    
     rm -rf /tmp/luci*
     exit 0
 EOF
