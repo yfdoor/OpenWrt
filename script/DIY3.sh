afc4a17fb235ffc60bec35ecb4bc8471fcbe4198
yfdoor <yfdoor@github.com>
2021-10-06 15:31:10 +0800

    update

--- script/DIY3.sh
+++ script/DIY3.sh
@@ -2,11 +2,13 @@
 # This is free software, lisence use MIT.
 # Copyright (C) https://github.com/yfdoor
 
-if [$REPO_URL = "https://github.com/coolsnowwolf/lede.git"]; then
+echo $REPO_URL
+echo $PKG_LEAN
+if [ $REPO_URL = "https://github.com/coolsnowwolf/lede.git" ]; then
     rm -rf package/lean
 fi          
 
-if [$REPO_URL != "https://github.com/coolsnowwolf/lede.git"] && [$PKG_LEAN = "true"]; then   
+if [ $REPO_URL != "https://github.com/coolsnowwolf/lede.git" ] && [ $PKG_LEAN = "true" ]; then   
     mkdir -p tools/ucl && wget -P tools/ucl https://raw.githubusercontent.com/coolsnowwolf/lede/master/tools/ucl/Makefile          
     mkdir -p tools/upx && wget -P tools/upx https://raw.githubusercontent.com/coolsnowwolf/lede/master/tools/upx/Makefile
     sed -i '23a\tools-y += ucl upx' tools/Makefile
