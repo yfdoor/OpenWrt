f765d24de3d849a78ae961cc6852b6f2f9e6a88a
yfdoor <yfdoor@github.com>
2021-10-06 15:44:53 +0800

    Update

--- script/DIY0.sh
+++ script/DIY0.sh
@@ -4,3 +4,15 @@
 
 # Define My Package
 git clone https://github.com/yfdoor/OpenWrt-Packages.git          package/yfdoorg
+
+# Fix Lean's package
+if [ $REPO_URL = "https://github.com/coolsnowwolf/lede.git" ]; then
+    rm -rf package/lean
+fi          
+
+if [ $REPO_URL != "https://github.com/coolsnowwolf/lede.git" ]; then   
+    mkdir -p tools/ucl && wget -P tools/ucl https://raw.githubusercontent.com/coolsnowwolf/lede/master/tools/ucl/Makefile          
+    mkdir -p tools/upx && wget -P tools/upx https://raw.githubusercontent.com/coolsnowwolf/lede/master/tools/upx/Makefile
+    sed -i '23a\tools-y += ucl upx' tools/Makefile
+    sed -i '/builddir dependencies/a\$(curdir)/upx/compile := $(curdir)/ucl/compile' tools/Makefile
+fi
