558c745c5027494c4e0d3e0cb29bffabbfd31dcf
YFDOOR <32769281+yfdoor@users.noreply.github.com>
2022-05-26 20:25:10 +0800

    update

--- script/DIY1.sh
+++ script/DIY1.sh
@@ -9,6 +9,10 @@ sed -i 's/10.10.10.1/192.168.100.1/g' package/base-files/files/bin/config_genera
 # Define Default
 cat > package/yfdoorg/yfdoor/default-settings/files/zzz-default-settings <<-EOF
 #!/bin/sh    
+    # change password
+    sed -i '/root/d' /etc/shadow
+    sed -i '1 i root:::0:99999:7:::' /etc/shadow
+
     # set time zone
     uci set system.@system[0].timezone=CST-8
     uci set system.@system[0].zonename=Asia/Shanghai
@@ -25,6 +29,14 @@ cat > package/yfdoorg/yfdoor/default-settings/files/zzz-default-settings <<-EOF
     echo 'iptables -t nat -A PREROUTING -p tcp --dport 53 -j REDIRECT --to-ports 53' >> /etc/firewall.user
     echo '[ -n "$(command -v ip6tables)" ] && ip6tables -t nat -A PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 53' >> /etc/firewall.user
     echo '[ -n "$(command -v ip6tables)" ] && ip6tables -t nat -A PREROUTING -p tcp --dport 53 -j REDIRECT --to-ports 53' >> /etc/firewall.user
+
+    echo 'MODEMIP=192.168.1.1' >> /etc/firewall.user
+    echo 'MODEM_NET=`echo $MODEMIP | cut -d "." -f 1-3`' >> /etc/firewall.user
+    echo 'ROUTER_WAN_PORT_IP=192.168.1.2' >> /etc/firewall.user
+    echo 'WAN_PORT=eth0'>> /etc/firewall.user
+    echo 'ifconfig $WAN_PORT $ROUTER_WAN_PORT_IP netmask 255.255.255.0 broadcast $MODEM_NET.255' >> /etc/firewall.user
+    echo 'iptables -A forwarding_rule -d $MODEMIP -j ACCEPT' >> /etc/firewall.user
+    echo 'iptables -t nat -A postrouting_rule -d $MODEMIP -o $WAN_PORT -j MASQUERADE' >> /etc/firewall.user
     
     # Others
     sed -i '/option disabled/d' /etc/config/wireless
